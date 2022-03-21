66ee923d18efdb08a9ec3903edface2379f058e9
RmnITLL <inetmilo@rambler.ru>
2022-03-21 10:57:37 +0300

    Добавили кое-что

--- src/main/java/lesson_1/Lesson_1.java
+++ src/main/java/lesson_1/Lesson_1.java
@@ -3,5 +3,6 @@ package lesson_1;
 public class Lesson_1 {
     public static void main(String[] args) {
         System.out.println("I'm lesson_1 branch!");
+        System.out.println("Кое-что еще добавили ");
     }
 }
