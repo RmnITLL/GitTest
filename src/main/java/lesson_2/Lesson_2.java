0041875e049b8b230270dd866a53171e4e5580b9
RmnITLL <inetmilo@rambler.ru>
2022-03-21 11:26:45 +0300

    HomeWork 2.1

--- src/main/java/lesson_2/Lesson_2.java
+++ src/main/java/lesson_2/Lesson_2.java
@@ -3,5 +3,6 @@ package lesson_2;
 public class Lesson_2 {
     public static void main(String[] args) {
         System.out.println("I'm lesson_2 branch!");
+        System.out.println("I'm lesson_2!");
     }
 }
